69bf4dedabae97905655f196e4d301949d75f171
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-02-27 21:50:02 -0300

    adiciona menu,lista e footer

--- Frontend/SistemaAgendamento/src/app/app.module.ts
+++ Frontend/SistemaAgendamento/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaClienteComponent } from './lista-cliente/lista-cliente/lista-cliente.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { MenuComponent } from './menu/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaClienteComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
