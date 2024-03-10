01b302ad06c1b7a58d05395f9810db436e0886dd
Leonardo Rossi <83621860+leorossiio@users.noreply.github.com>
2024-03-10 18:38:48 -0300

    home-pacotes: cria os cards e a função

--- Frontend/SistemaAgendamento/src/app/homepage/home-pacotes/home-pacotes.component.ts
+++ Frontend/SistemaAgendamento/src/app/homepage/home-pacotes/home-pacotes.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePacotesComponent {
 
+  mensaisSelecionado?: boolean = true;
+  anuaisSelecionado?: boolean = false;
+
+  pacotes = {
+    afrodite: 100,
+    hercules: 150,
+    hades: 200,
+    zeus: 300
+  };
+
+  alterarTipo() {
+    this.mensaisSelecionado = !this.mensaisSelecionado;
+    this.anuaisSelecionado = !this.anuaisSelecionado;
+
+    if (this.anuaisSelecionado) {
+      // Reduz 15% do valor total para cada pacote
+      this.pacotes.afrodite = this.pacotes.afrodite * 0.80;
+      this.pacotes.hercules = this.pacotes.hercules * 0.80;
+      this.pacotes.hades = this.pacotes.hades * 0.80;
+      this.pacotes.zeus = this.pacotes.zeus * 0.80;
+    } else {
+      // Se o tipo for mensal, restaure os valores originais
+      this.pacotes.afrodite = 100;
+      this.pacotes.hercules = 150;
+      this.pacotes.hades = 200;
+      this.pacotes.zeus = 300;
+    }
+  }
 }
