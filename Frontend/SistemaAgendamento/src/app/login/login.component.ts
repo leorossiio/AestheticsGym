9f33a077b206003079a7a19a5a555bf1bc8eb693
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-03-19 21:19:25 -0300

    tela de login

--- Frontend/SistemaAgendamento/src/app/login/login.component.ts
+++ Frontend/SistemaAgendamento/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario = {
+    login: '',
+    senha: ''
+  }
+
+  loginAuth: string = "admin";
+  senhaAuth: string = "admin";
+  constructor(
+    private router : Router
+    )
+    {}
+  fazerLogin() {
+    if (this.usuario.login === this.loginAuth && this.usuario.senha === this.senhaAuth) {
+      this.router.navigate(['/home']);
+      alert("Login feito");
+    }
+    else{
+      alert("Acesso recusado");
+    }
+  }
 
 }
