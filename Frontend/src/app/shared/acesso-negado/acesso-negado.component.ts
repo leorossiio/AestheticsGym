b0fc2ebe11733ba042150fff07d1c627d4d1c80e
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-12 23:50:42 -0300

    adiciona components para erros (404) e (403)

---
+++ Frontend/src/app/shared/acesso-negado/acesso-negado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-acesso-negado',
+  templateUrl: './acesso-negado.component.html',
+  styleUrls: ['./acesso-negado.component.css']
+})
+export class AcessoNegadoComponent {
+  constructor(
+
+    private router: Router
+  ) { }
+
+  redirectToUserRole(): void {
+    this.router.navigate(['/home']);
+  }
+
+}
