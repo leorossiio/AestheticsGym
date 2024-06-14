8ab68fb30cbed68083c1179c8944d8d9f798c14c
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-13 22:09:57 -0300

    Ajusta editar usuario e menuAuth

--- Frontend/src/app/tela-autenticada/components/lista-aluno/lista-aluno.component.ts
+++ Frontend/src/app/tela-autenticada/components/lista-aluno/lista-aluno.component.ts
@@ -8,55 +8,57 @@ import { UserService } from '../../../services/usuario.service';
 })
   
 export class ListaAlunoComponent implements OnInit {
-    users: any[] = []; // Inicializando como um array vazio
+  users: any[] = []; 
   
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
   
-    ngOnInit(): void {
-      this.carregarUsuarios();
-    }
-  
-    carregarUsuarios(): void {
-      this.userService.listarUsuarios().subscribe(
-        (data) => {
-          this.users = data;
-        },
-        (error) => {
-          console.error('Erro ao carregar usuários:', error);
-        }
-      );
-    }
-  
-    toggleEdit(user: any): void {
-      if (user.isEditing) {
-        this.salvarEdicao(user);
-      }
-      user.isEditing = !user.isEditing;
-    }
-  
-    salvarEdicao(user: any): void {
-      this.userService.editarUsuario(user).subscribe(
-        (data) => {
-          console.log('Usuário editado com sucesso:', data);
-        },
-        (error) => {
-          console.error('Erro ao editar usuário:', error);
-        }
-      );
-    }
-  
-    deletarUsuario(user: any): void {
-      this.userService.deletarUsuario(user.idUser).subscribe(
-        () => {
-          const index = this.users.indexOf(user);
-          if (index !== -1) {
-            this.users.splice(index, 1);
-          }
-          console.log('Usuário deletado com sucesso');
-        },
-        (error) => {
-          console.error('Erro ao deletar usuário:', error);
-        }
-      );
-    }
+  ngOnInit(): void {
+    this.carregarUsuarios();
   }
+  
+  carregarUsuarios(): void {
+    this.userService.listarUsuarios().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => {
+        console.error('Erro ao carregar usuários:', error);
+      }
+    );
+  }
+  
+  toggleEdit(user: any): void {
+    if (user.isEditing) {
+      alert("Usuário salvo com sucesso!")
+      this.salvarEdicao(user);
+    }
+    user.isEditing = !user.isEditing;
+  }
+  
+  salvarEdicao(user: any): void {
+    this.userService.editarUsuario(user).subscribe(
+      (data) => {
+        console.log('Usuário editado com sucesso:', data);
+        this.carregarUsuarios(); // Atualiza a tabela após salvar a edição
+      },
+      (error) => {
+        console.error('Erro ao editar usuário:', error);
+      }
+    );
+  }
+  
+  deletarUsuario(user: any): void {
+    this.userService.deletarUsuario(user.idUser).subscribe(
+      () => {
+        const index = this.users.indexOf(user);
+        if (index !== -1) {
+          this.users.splice(index, 1);
+        }
+        console.log('Usuário deletado com sucesso');
+      },
+      (error) => {
+        console.error('Erro ao deletar usuário:', error);
+      }
+    );
+  }
+}
