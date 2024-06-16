78fb8187e9f3b65ba2daa7a9a912b45cc7f44384
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-06-16 20:49:38 -0300

    Ajusta a listagem da tela de cardápio com base no usuário logado

--- Frontend/src/app/services/dieta.service.ts
+++ Frontend/src/app/services/dieta.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
+import { AuthService } from '../guard/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { environment } from 'src/environment/environment';
 export class DietaService {
   private baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   private getAuthHeaders(): HttpHeaders {
     const token = localStorage.getItem('token');
@@ -17,20 +18,22 @@ export class DietaService {
   }
 
   listarDietas(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/listarDietas`, { headers: this.getAuthHeaders() });
+    return this.http.get<any[]>(`${this.baseUrl}/dieta/listarDietas`, { headers: this.getAuthHeaders() });
   }
 
-  
+  listarDietaByUser(idUser: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/dieta/listarDietasPorUsuario/${idUser}`, { headers: this.getAuthHeaders() });
+  }
 
   criarDieta(dietaData: any): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/cadastroDieta`, dietaData, { headers: this.getAuthHeaders() });
+    return this.http.post<any>(`${this.baseUrl}/dieta/cadastroDieta`, dietaData, { headers: this.getAuthHeaders() });
   }
 
   editarDieta(idDieta: string, dietaData: any): Observable<any> {
-    return this.http.put<any>(`${this.baseUrl}/editarDieta/${idDieta}`, dietaData, { headers: this.getAuthHeaders() });
+    return this.http.put<any>(`${this.baseUrl}/dieta/editarDieta/${idDieta}`, dietaData, { headers: this.getAuthHeaders() });
   }
 
   deletarDieta(idDieta: string): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/deletarDieta/${idDieta}`, { headers: this.getAuthHeaders() });
+    return this.http.delete<any>(`${this.baseUrl}/dieta/deletarDieta/${idDieta}`, { headers: this.getAuthHeaders() });
   }
 }
--- Frontend/src/app/tela-autenticada/components/nutricao-lista/nutricao-lista.component.ts
+++ Frontend/src/app/tela-autenticada/components/nutricao-lista/nutricao-lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/guard/auth.service';
+import { DietaService } from 'src/app/services/dieta.service';
 
 interface Refeicao {
   nome: string;
@@ -22,228 +23,53 @@ type PlanosSemana = {
 })
 export class NutricaoListaComponent implements OnInit {
   userRole: string | null = null;
-  diaSelecionado: string = 'Segunda-feira';
-  diasSemana: string[] = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
-
-  constructor(private authService: AuthService) { }
+  diaSelecionado: string = 'segunda-feira';
+  diasSemana: string[] = ['segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
 
   aluno = {
     nome: this.authService.getUserName(),
     email: this.authService.getUserEmail()
   };
 
-  planosSemana: PlanosSemana = {
-    'Segunda-feira': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Omelete de claras com espinafre.',
-          calorias: 150 
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Iogurte grego com mel e nozes.',
-          calorias: 100
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Peito de frango grelhado com batata-doce.',
-          calorias: 350
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Smoothie de frutas vermelhas.',
-          calorias: 150
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Sopa de legumes com quinoa.',
-          calorias: 200
-        }
-      ]
-    },
-    'Terça-feira': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Aveia com banana e morango.',
-          calorias: 200
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Maçã com pasta de amendoim.',
-          calorias: 120
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Salada de quinoa com legumes frescos.',
-          calorias: 250
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Barrinha de cereais.',
-          calorias: 100
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Peixe grelhado com vegetais.',
-          calorias: 300
-        }
-      ]
-    },
-    // Continue adicionando planos para os outros dias da semana
-    'Quarta-feira': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Panqueca de aveia com mel.',
-          calorias: 180
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Laranja e castanhas.',
-          calorias: 110
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Macarrão integral com molho de tomate e frango.',
-          calorias: 320
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Iogurte natural com frutas.',
-          calorias: 130
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Salada de atum com grão-de-bico.',
-          calorias: 210
-        }
-      ]
-    },
-    'Quinta-feira': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Smoothie verde com espinafre e banana.',
-          calorias: 170
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Biscoitos integrais com queijo cottage.',
-          calorias: 120
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Arroz integral com legumes e carne magra.',
-          calorias: 330
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Pêra e nozes.',
-          calorias: 140
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Sopa de lentilha com cenoura.',
-          calorias: 220
-        }
-      ]
-    },
-    'Sexta-feira': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Iogurte com granola.',
-          calorias: 160
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Banana com aveia.',
-          calorias: 130
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Quiche de espinafre com salada.',
-          calorias: 280
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Suco de laranja natural.',
-          calorias: 100
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Frango ao curry com arroz integral.',
-          calorias: 300
-        }
-      ]
-    },
-    'Sábado': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Torrada integral com abacate.',
-          calorias: 200
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Morango com iogurte natural.',
-          calorias: 110
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Salmão grelhado com batatas.',
-          calorias: 350
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Suco de beterraba e cenoura.',
-          calorias: 90
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Pizza de massa integral com vegetais.',
-          calorias: 250
-        }
-      ]
-    },
-    'Domingo': {
-      refeicoes: [
-        {
-          nome: 'Café da manhã',
-          descricao: 'Vitamina de frutas com aveia.',
-          calorias: 180
-        },
-        {
-          nome: 'Lanche da manhã',
-          descricao: 'Tangerina e amêndoas.',
-          calorias: 100
-        },
-        {
-          nome: 'Almoço',
-          descricao: 'Carne assada com legumes.',
-          calorias: 340
-        },
-        {
-          nome: 'Lanche da tarde',
-          descricao: 'Suco verde detox.',
-          calorias: 80
-        },
-        {
-          nome: 'Jantar',
-          descricao: 'Wrap integral com frango e salada.',
-          calorias: 240
-        }
-      ]
-    }
-  };
+  planosSemana: PlanosSemana = {};
 
-  get planoDoDia(): PlanoNutricional {
-    return this.planosSemana[this.diaSelecionado];
-  }
+  constructor(private authService: AuthService, private dietaService: DietaService) { }
 
   ngOnInit(): void {
     this.userRole = this.authService.getUserRole();
+    this.carregarDietas();
+    console.log('Dia selecionado:', this.diaSelecionado); // Adicionar console.log aqui
+  }
+
+  carregarDietas(): void {
+    const idUser = this.authService.getUserId(); // Obter o ID do usuário autenticado
+    this.dietaService.listarDietaByUser(idUser).subscribe( // Passar o idUser como argumento
+      (data: any[]) => {
+        this.processarDietas(data);
+      },
+      (error) => {
+        console.error('Erro ao carregar dietas:', error);
+      }
+    );
+  }
+
+  processarDietas(dietas: any[]): void {
+    this.planosSemana = dietas.reduce((acc, dieta) => {
+      const dia = dieta.diaDaSemana.toLowerCase(); // Usar toLowerCase para padronizar
+      if (!acc[dia]) {
+        acc[dia] = { refeicoes: [] };
+      }
+      acc[dia].refeicoes.push({
+        nome: dieta.nome,
+        descricao: dieta.descricao,
+        calorias: dieta.calorias
+      });
+      return acc;
+    }, {} as PlanosSemana);
+    console.log('Planos de semana:', this.planosSemana); // Adicionar console.log aqui
+  }
+  
+  get planoDoDia(): PlanoNutricional {
+    return this.planosSemana[this.diaSelecionado] || { refeicoes: [] };
   }
 }
